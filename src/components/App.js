725053029e8be725fd2403da656f7eda11f7c8a3
Demo <siddy5691@gmail.com>
2021-02-18 18:44:38 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,43 @@
-import React, {Component, useState} from "react";
+import React, { Component, useState, useEffect } from "react";
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi'
 import '../styles/App.css';
 
-const App = () => {
+const App = ({ slides }) => {
+  const [index, setIndex] = useState(0);
+  const [data, setData] = useState('')
+  const [disabledBack, setDisabledBack] = useState(true)
+  const [disabledNext, setDisabledNext] = useState(false)
+
+  const getIndex = () => {
+    setData(slides[index])
+  }
+
+  useEffect(() => {
+    if (index === 0) {
+      setDisabledBack(true)
+      setDisabledNext(false)
+    } else if (index == slides.length - 1) {
+      setDisabledNext(true)
+    } else {
+      setDisabledNext(false)
+      setDisabledBack(false)
+    }
+    getIndex()
+  }, [index])
+
   return (
-    <></>
+    <>
+      <div className="section-center">
+        <article>
+          <h1 data-testid="title" className="title">{data.title}</h1>
+          <p data-testid="text" className="text">{data.text}</p>
+        </article>
+
+        <button disabled={disabledBack} data-testid="button-prev" className="prev" onClick={() => setIndex(index - 1)}><FiChevronLeft /></button>
+        <button disabled={disabledNext} data-testid="button-next" className="next" onClick={() => setIndex(index + 1)}><FiChevronRight /></button>
+        <button disabled={disabledBack} data-testid="button-restart" className="next" onClick={() => setIndex(0)}>Restart</button>
+      </div>
+    </>
   )
 }
 
